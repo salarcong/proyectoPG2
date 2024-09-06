af95db9a0217b9030d1c5e88d9fc2f5bef6d3e9e
salarcong <salarcong@miumg.edu.gt>
2024-09-05 23:14:49 -0600

    agregación de validaciones

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -7,12 +7,14 @@ import {
     getTask, 
     updateTasks 
     } from "../controllers/tasks.controller.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
 
 
 const router = Router();
 
 router.get("/tasks", authRequired, getTasks);
-router.post("/tasks", authRequired, createTasks);
+router.post("/tasks", authRequired, validateSchema(createTaskSchema), createTasks);
 router.delete("/tasks/:id", authRequired, deleteTasks);
 router.get("/tasks/:id", authRequired, getTask);
 router.put("/tasks/:id", authRequired, updateTasks);
