70823f6f89c1dc5867bd71a822e299e50ed0f7d0
salarcong <salarcong@miumg.edu.gt>
2024-09-05 21:43:13 -0600

    node_module

--- Proyecto de Graduación/src/index.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js'
-import {connectDB} from './db.js'
-
-/*se conecta a la base de datos*/
-connectDB();
-
-/*se crea el puerto del servidor*/
-app.listen(3000)
-console.log('Server on port', 3000)
