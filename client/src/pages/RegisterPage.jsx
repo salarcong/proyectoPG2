aa34c3b44c6d92303179712be19cb90561780bdc
salarcong <salarcong@miumg.edu.gt>
2024-09-11 20:17:57 -0600

    cambio registerpage.jsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -5,17 +5,19 @@ function RegisterPage() {
 
     const {register, handleSubmit} = useForm();
 
-  return (
-    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
-      <form onSubmit={handleSubmit(async(values) => {
-        console.log(values);
+    const onSubmit = handleSubmit(async(values) => {
         const res = await registerRequest(values);
         console.log(res);
-      })}>
-        <input type="text" 
-            {... register('username', {required: true})} 
-            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-        placeholder='Username'
+    });
+
+  return (
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+              {... register('username', {required: true})} 
+              className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder='Username'
         />
         
         <input type="text" 
